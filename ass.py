8c42ebb05d7fcb4dc9d3abe33056be3d0bc9cdbc
pivaszbs <zhychek1@yandex.ru>
2019-10-09 16:03:41 +0300

    kek

--- ass.py
+++ ass.py
@@ -1,6 +1,7 @@
 import numpy as np
 from typing import List
 import os
+import struct
 
 def encode(s):
     buffer = ''
@@ -20,44 +21,63 @@ def encode(s):
     return ans
 
 def left(n):
-    def leftLen(n):
-        if (n == 0):
-            return 1
+    if n == 0:
+        return '10000000'
+    s = ''
+    flag = True
+    while n > 0:
+        s = '{:7b}'.format(n & 0b01111111) + s
+        if flag:
+            s = '1' + s
+            flag = False
         else:
-            return np.ceil(np.log2(n))
-    
-    s = "{0:b}".format(n)
-
-    while (len(s) < leftLen(n)):
-        s = '0' + s
-
+            s = '0' + s
+        n >>= 7
+    s = s.replace(' ', '0')
     return s
 
 def encodeInBinary(encoded):
     ans = ''
     s = ''
     for node in encoded:
-        right = str("{0:b}".format(ord(node[1])))
-        # right = '0' if node[1] == 'a' else '1'
-        s += left(node[0]) + right  + ''
+        right = str("{:8b}".format(ord(node[1]))).replace(' ', '0')
+        s += left(node[0]) + right 
     return s
 
 def decodeFromBinary(s):
-    a = s.strip().split(' ')
+    a = bytearray(s)
+    i = 0
+    j = 0
+    left = 0
     ans = []
-    for node in a:
-        right = chr(int(node[-7:], 2))
-        left = int(node[:-7], 2)
-        ans.append((left, right))
-    return decode(ans)   
+    next_char = False
+    while i < len(a):
+        if next_char:
+            right = chr(a[i])
+            ans.append((left, right))
+            next_char = False
+            left = 0
+            j = 0
+        elif a[i] & 128 == 128:
+            while j >= 0:
+                left += (a[i - j] & 127) << (j * 7)
+                j -= 1
+            j = 0
+            next_char = True
+        else:
+            j+=1
+        
+        i+=1
+    return decode(ans)
 
 def decode(encoded):
     phrases = ['']
     ans = ""
     for node in encoded:
-        key = phrases[node[0]]
-        if not key:
+        if node[0] >= len(phrases):
             key = ''
+        else:
+            key = phrases[node[0]]
         word = key + node[1]
         ans += word
         phrases.append(word)
@@ -89,14 +109,18 @@ def compress_files(file_names: List[str], prefix: str, save_prefix: str):
         f = open(compressed_filename(save_prefix, delim, path), 'wb')
         f.write(bytes(int(s[i : i + 8], 2) for i in range(0, len(s), 8)))
 
+def decompressed_filename(prefix, delim, path):
+    ans = prefix + delim + path
+    dotidx = ans.rfind('.')
+    return ans[:dotidx] + 'Decompressed' + ans[dotidx:]
+
 def decompress_file(filename: str) -> List[tuple]:
     """
-    Calculate the entropy of the file
     :param filename: name of file
     :return:
     """
     with open(filename, "rb") as f:
-        return decodeFromBinary(decode(f.read()))   
+        return decodeFromBinary(f.read())
 
 def decompress_files(file_names: List[str], prefix: str):
     """
@@ -114,7 +138,9 @@ def decompress_files(file_names: List[str], prefix: str):
         filename = prefix + delim + path
         s = decompress_file(compressed_filename(prefix, delim, path))
         f = open(decompressed_filename(prefix, delim, path), 'wb')
-        f.write(bytes(int(s[i : i + 8], 2) for i in range(0, len(s), 8)))
+        b = bytearray()
+        b.extend(map(ord, s))
+        f.write(b)
 
 
 if __name__ == '__main__':
@@ -123,11 +149,12 @@ if __name__ == '__main__':
     dirs = os.listdir(dir)
     delim = '/'
     for d in dirs:
+        if d == 'jpg':
+            continue
         path = dir + delim + d
         save_path = compressDir + delim + d
         files = os.listdir(path)
         if not os.path.exists(save_path):
             os.makedirs(save_path)
-        # compress_files(files, path, save_path)
+        compress_files(files, path, save_path)
         decompress_files(files, save_path)
-        
